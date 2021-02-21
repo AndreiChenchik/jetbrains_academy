6cfbd1028124819b3b34d33fe4b5b04504a8da9b
Andrei Chenchik <andrei@chenchik.me>
2021-02-21 15:19:50 +0100

    Simple Banking System: Add more type hints to bank.py

--- simple_banking_system/banking.py
+++ simple_banking_system/banking.py
@@ -1,11 +1,11 @@
 import random
-from typing import Optional
+from typing import Optional, Tuple, List
 from enum import Enum
 import sqlite3
 
 
 class Bank(object):
-    def __init__(self, bank_iin: int, database: sqlite3.Connection):
+    def __init__(self, bank_iin: int, database: sqlite3.Connection) -> None:
         self.bank_iin = bank_iin
         self.database = database
         self.cur: sqlite3.Cursor = self.database.cursor()
@@ -21,7 +21,7 @@ class Bank(object):
         self.cur.execute(create_table)
         self.database.commit()
 
-    def create_account(self) -> ('BankAccount', str):
+    def create_account(self) -> Tuple['BankAccount', str]:
         account_number = random.randint(0, 999999999)
 
         self.cur.execute(f"""
@@ -46,7 +46,8 @@ class Bank(object):
 
         return new_account, pin
 
-    def get_account(self, card_number, pin) -> Optional['BankAccount']:
+    def get_account(self, card_number: str,
+                    pin: str) -> Optional['BankAccount']:
         try:
             account_number = int(card_number[6:15])
             self.cur.execute(f"""
@@ -75,26 +76,25 @@ class Bank(object):
         account.balance = new_balance
         return account
 
-    def close_account(self, account):
+    def close_account(self, account: 'BankAccount') -> None:
         self.cur.execute(f"""
             DELETE FROM card WHERE id = {account.account_number};
         """)
         self.database.commit()
 
     @staticmethod
-    def luhn_card_verification(destination_card):
-        card_number = destination_card[:15]
-        card_number = [int(dig)
-                       for dig in card_number]
-        card_number = [dig if ind % 2 else dig * 2
-                       for ind, dig in enumerate(card_number)]
+    def luhn_card_verification(destination_card: str) -> bool:
+        card_number: str = destination_card[:15]
+        card_digits: List[int] = [int(dig) for dig in card_number]
+        card_digits = [dig if ind % 2 else dig * 2
+                       for ind, dig in enumerate(card_digits)]
         sum_of_digits = sum([dig - 9 if dig > 9 else dig
-                             for dig in card_number])
+                             for dig in card_digits])
         return \
             destination_card[:15] + str((10 - sum_of_digits % 10) % 10) \
             == destination_card
 
-    def is_card_exists(self, destination_card):
+    def is_card_exists(self, destination_card: str) -> bool:
         try:
             account_number = int(destination_card[6:15])
 
@@ -110,7 +110,9 @@ class Bank(object):
         except (ValueError, TypeError):
             return False
 
-    def transfer_funds(self, account, destination_card, amount):
+    def transfer_funds(self, account: 'BankAccount',
+                       destination_card: str,
+                       amount: float) -> 'BankAccount':
         new_balance = account.balance - amount
 
         destination_account = int(destination_card[6:15])
@@ -158,7 +160,7 @@ class AccountMenu(Enum):
 
 
 class ConsoleUI(object):
-    def __init__(self):
+    def __init__(self) -> None:
         self.level: MenuLevel = MenuLevel.TOP
 
     def display_menu(self) -> None:
@@ -178,7 +180,7 @@ class ConsoleUI(object):
             print(f"{TopMenu.EXIT.value}. Exit")
 
     @staticmethod
-    def message_show_new_card(card_number, pin) -> None:
+    def message_show_new_card(card_number: str, pin: str) -> None:
         print("\nYour card has been created")
         print("Your card number:")
         print(card_number)
@@ -214,7 +216,7 @@ class ConsoleUI(object):
         print("\nYou have successfully logged out!")
 
     @staticmethod
-    def message_show_balance(balance) -> None:
+    def message_show_balance(balance: float) -> None:
         print(f"\nBalance: {balance}")
 
     @staticmethod
@@ -265,7 +267,7 @@ class ConsoleUI(object):
 
 
 class Controller(object):
-    def __init__(self, ui, bank):
+    def __init__(self, ui: ConsoleUI, bank: Bank) -> None:
         self.ui: ConsoleUI = ui
         self.bank: Bank = bank
         self.account: Optional[BankAccount] = None
@@ -302,7 +304,7 @@ class Controller(object):
         except KeyError:
             self.ui.message_wrong_command()
 
-    def handler_income(self):
+    def handler_income(self) -> None:
         self.ui.message_enter_income()
         try:
             self.account = self.bank.add_funds(self.account, float(input()))
@@ -310,7 +312,7 @@ class Controller(object):
         except ValueError:
             self.ui.message_wrong_command()
 
-    def handler_transfer(self):
+    def handler_transfer(self) -> None:
         self.ui.message_enter_card_for_transfer()
         destination_card = input()
 
@@ -329,16 +331,16 @@ class Controller(object):
                                                         amount)
                 self.ui.message_transfer_success()
 
-    def handler_close_account(self):
+    def handler_close_account(self) -> None:
         self.bank.close_account(self.account)
         self.ui.level = MenuLevel.TOP
         self.ui.message_account_closed()
 
-    def handler_create_account(self):
+    def handler_create_account(self) -> None:
         user_account, pin = self.bank.create_account()
         self.ui.message_show_new_card(user_account.get_card_number(), pin)
 
-    def handler_login(self):
+    def handler_login(self) -> None:
         self.ui.message_enter_card()
         user_card = input()
         self.ui.message_enter_pin()
@@ -352,34 +354,33 @@ class Controller(object):
         else:
             self.ui.message_login_failed()
 
-    def handler_balance(self):
+    def handler_balance(self) -> None:
         self.ui.message_show_balance(self.account.balance)
 
-    def handler_logout(self):
+    def handler_logout(self) -> None:
         self.ui.level = MenuLevel.TOP
         self.ui.message_logout_success()
 
-    def handler_exit(self):
+    def handler_exit(self) -> None:
         self.ui.message_exit_warning()
         exit()
 
 
 class BankAccount(object):
     def __init__(self, account_number: int,
-                 account_bank: Bank, balance: float):
+                 account_bank: Bank, balance: float) -> None:
         self.account_number = account_number
         self.bank_iin = account_bank.bank_iin
         self.balance = balance
 
     def get_luhn_ending(self) -> str:
-        card_number = f"{self.bank_iin}" \
+        card_number: str = f"{self.bank_iin}" \
                       f"{self.account_number:09d}"
-        card_number = [int(dig)
-                       for dig in card_number]
-        card_number = [dig if ind % 2 else dig * 2
-                       for ind, dig in enumerate(card_number)]
+        card_digits: List[int] = [int(dig) for dig in card_number]
+        card_digits = [dig if ind % 2 else dig * 2
+                       for ind, dig in enumerate(card_digits)]
         sum_of_digits = sum([dig - 9 if dig > 9 else dig
-                             for dig in card_number])
+                             for dig in card_digits])
         return str((10 - sum_of_digits % 10) % 10)
 
     def get_card_number(self) -> str:
